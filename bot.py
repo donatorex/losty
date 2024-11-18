254eb464ec7f824d554c84d75d18453ad6173a83
donatorex <roakozlov@gmail.com>
2024-11-19 02:15:59 +0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,250 @@
+"""
+Losty – telegram bot for finding lost pets.
+
+URL: https://t.me/losty_pets_bot
+
+This script is the main entry point for the Losty telegram bot. It continuously updates the database
+with new posts, handles incoming messages and sends matches to the user.
+
+Author: Roman Kozlov
+Github: https://github.com/donatorex
+"""
+
+import os
+import random
+import sqlite3
+import threading
+import time
+from datetime import datetime
+
+import telebot
+from PIL import Image
+
+from losty.main import LostyFinder
+
+
+API_TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
+bot = telebot.TeleBot(API_TOKEN)
+
+losty = LostyFinder()
+user_data = {}
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message: telebot.types.Message) -> None:
+    """
+    Handle the '/start' command and send a welcome message.
+    """
+    bot.send_message(message.chat.id, 'Отлично, давай начнём! Отправь мне фотографию хвостика🐾')
+
+
+@bot.message_handler(commands=['test'])
+def send_test(message):
+    bot.reply_to(message, "Test mode")
+
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message: telebot.types.Message) -> None:
+    """
+    Handle an incoming photo message, download the image and start a search for matches.
+
+    :param message: The incoming message containing the photo.
+    """
+    try:
+        file_info = bot.get_file(message.photo[-1].file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        bot.send_message(message.chat.id, 'Ищу совпадения...')
+
+        input_image_path = f"data/temp/{message.chat.id}_temp_image.jpg"
+        with open(input_image_path, 'wb') as new_file:
+            new_file.write(downloaded_file)
+
+        pages = losty.find_matches(input_image_path)
+        user_data[message.chat.id] = {'pages': pages, 'current_page': 1}
+
+        send_matches(message.chat.id, pages, 1)
+        os.remove(input_image_path)
+    except Exception as e:
+        print(f"Error occurred with bot: {e}")
+        bot.send_message(message.chat.id, "Произошла техническая ошибка. Попробуйте позже.")
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("next_page_"))
+def handle_page_change(call: telebot.types.CallbackQuery) -> None:
+    """
+    Handle a callback query to change the page of matches.
+
+    :param call: The incoming callback query.
+    """
+    page_number = int(call.data.split("_")[2])
+    chat_id = call.message.chat.id
+    if chat_id in user_data:
+        pages = user_data[chat_id]['pages']
+        send_matches(chat_id, pages, page_number)
+
+
+def add_logo_to_image(image_path: str, group: str) -> Image:
+    """
+    Add a logo to an image.
+
+    :param image_path: str - The file path to the image.
+    :param group: str - The name of the group.
+    :return: Image – The image with the added logo.
+    """
+    image = Image.open(image_path)
+
+    logo = Image.open(f"{group}/{group}_profile_pic.jpg").convert('RGBA')
+
+    logo_size_ratio = 0.15
+    logo_width = int(image.width * logo_size_ratio)
+    logo_height = int(logo.height * (logo_width / logo.width))
+
+    logo_resized = logo.resize((logo_width, logo_height), Image.Resampling.LANCZOS)
+
+    image.paste(logo_resized, (0, 0), logo_resized)
+
+    return image
+
+
+def send_matches(chat_id: int, pages: dict, page_number: int) -> None:
+    """
+    Send a message with matches found in the database.
+
+    :param chat_id: int - The telegram chat id to send the message to.
+    :param pages: dict - The dictionary of matches paginated by page number.
+    :param page_number: int - The number of the page to send.
+    """
+    try:
+        # Send a typing action to the user.
+        bot.send_chat_action(chat_id, 'upload_photo')
+        media = []
+
+        # Iterate over the matches in the current page.
+        for key, value in pages[page_number].items():
+            shortcode = key
+            group = value[0]
+            date = datetime.fromisoformat(value[1]).astimezone()
+            image_path = value[2]
+            match_percentage = value[3]
+
+            caption = get_caption(image_path)
+            desc = build_description(group, shortcode, date, caption, match_percentage)
+
+            image = add_logo_to_image(image_path, group)
+            media.append(telebot.types.InputMediaPhoto(media=image, caption=desc))
+
+        # Send the media group to the user.
+        bot.send_media_group(chat_id, media)
+
+        # Create the keyboard for the user.
+        keyboard = []
+        if page_number < len(pages):
+            keyboard.append([
+                telebot.types.InlineKeyboardButton("Найти еще совпадения", callback_data=f"next_page_{page_number + 1}")
+            ])
+
+        # Send the message with the keyboard.
+        reply_markup = telebot.types.InlineKeyboardMarkup(keyboard)
+        bot.send_message(chat_id, f"Страница {page_number} из 10", reply_markup=reply_markup)
+    except Exception as e:
+        print(f"Error occurred with bot: {e}")
+        bot.send_message(chat_id, "Произошла техническая ошибка. Попробуйте позже.")
+
+
+def get_caption(image_path: str) -> str:
+    """
+    Get the caption of an image from a file.
+
+    :param image_path: str - The file path to the image.
+    :return: str - The caption of the image.
+    """
+    caption_filename = image_path.split('_UTC')[0] + '_UTC.txt'
+    if os.path.exists(caption_filename):
+        with open(f"{caption_filename}", 'r', encoding='utf-8') as caption_file:
+            return caption_file.read()
+    return 'Описание отсутствует'
+
+
+def build_description(group: str, shortcode: str, date: datetime, caption: str, match_percentage: float) -> str:
+    """
+    Build the description of a match.
+
+    :param group: str - The name of the group.
+    :param shortcode: str - The shortcode of the post.
+    :param date: datetime - The date the post was published.
+    :param caption: str - The caption of the post.
+    :param match_percentage: float - The percentage of match.
+    :return: str - The description of the match.
+    """
+    description = f"""
+--- Группа ---
+@{group}
+
+--- Дата публикации ---
+{date}
+
+--- Ссылка на пост ---
+https://www.instagram.com/{group}/p/{shortcode}
+
+--- Совпадение ---
+{match_percentage:.2%}
+
+--- Описание ---
+{caption}
+"""
+
+    if len(description) > 1024:
+        return description[:1021] + '...'
+    else:
+        return description
+
+
+def update_data() -> None:
+    """
+    Continuously update the database with new posts and log the number of posts added.
+
+    :return: None
+    """
+    while True:
+        conn = sqlite3.connect('data/losty_db.db')
+        cur = conn.cursor()
+        try:
+            # Retrieve the minimum start date from posts.
+            cur.execute('SELECT MAX(date) FROM posts GROUP BY group_id')
+            start_date = min(cur.fetchall())[0]
+
+            # Get the count of posts before update.
+            cur.execute('SELECT COUNT(*) FROM posts;')
+            count_before = cur.fetchone()[0]
+
+            # Update data starting from the earliest post date or default to a full update.
+            if start_date:
+                losty.update_data(start_date=datetime.fromisoformat(start_date).replace(tzinfo=None))
+            else:
+                losty.update_data()
+
+            # Get the count of posts after update.
+            cur.execute('SELECT COUNT(*) FROM posts;')
+            count_after = cur.fetchone()[0]
+
+            # Log the number of posts added to the database.
+            print(f"Database has been updated (added {int(count_after) - int(count_before)} post(s))")
+            print('------------------------------')
+
+        except Exception as e:
+            print(f"An error occurred during the update: {e}")
+
+        finally:
+            # Ensure the cursor and connection are closed.
+            cur.close()
+            conn.close()
+
+        # Sleep for a random interval before the next update cycle.
+        time.sleep(random.randint(3400, 3800))
+
+
+if __name__ == "__main__":
+    update_thread = threading.Thread(target=update_data, daemon=True)
+    update_thread.start()
+    bot.infinity_polling()
---
+++ main.py
@@ -0,0 +1,448 @@
+"""
+LostyFinder: Instagram-Based Image Matching and Database Management
+
+This script defines a Python module for managing a database of Instagram posts and images,
+primarily focused on lost and found pet groups. It utilizes Instagram scraping, image
+embedding generation using a ResNet50 model, and K-Nearest Neighbors (KNN) for finding
+similar images.
+
+The module provides functionality to:
+
+    — Upload data about posts in specified Instagram groups and auto-update in a set period.
+    — Extract image embeddings using a pre-trained ResNet50 deep learning model.
+    — Add and manage groups, posts, images and their embeddings in a SQLite database.
+    — Cleaning the database and storage from old posts (older than half a year).
+    — Search for the most relevant posts for an input image by embeddings using the unsupervised NearestNeighbors model.
+
+Main Classes and Functions:
+    — `LostyFinder`: Class responsible for downloading Instagram data, managing the database,
+      generating image embeddings, and performing image matching.
+    — `add_group`: Function to add a group to the database.
+    — `add_post`: Function to add a post record to the database.
+    — `add_image`: Function to add an image record to the database.
+    — `check_shortcode`: Function to check if a shortcode exists in the database.
+    — `cleanup_data`: Function to clean up old image and post data.
+    — `knn_refit`: Method to refit the KNN model using embeddings from the database.
+    — `find_matches`: Method to find and paginate image matches for a given input image.
+
+Dependencies:
+    — `instaloader`: For downloading Instagram posts and profile data.
+    — `numpy`: For numerical operations and array handling.
+    — `requests`: For handling HTTP requests.
+    — `PIL`: For image processing.
+    — `sklearn`: For machine learning models, including KNN.
+    — `tensorflow.keras`: For deep learning model ResNet50.
+    — `sqlite3`: For database interactions.
+
+Note: This script assumes the existence of an SQLite database at 'data/losty_db.db' and
+appropriate database schema for storing group, post and image records.
+
+Author: Roman Kozlov
+Github: https://github.com/donatorex
+"""
+
+import io
+import json
+import lzma
+import os
+import sqlite3
+from datetime import datetime, timezone, timedelta
+
+import instaloader
+import numpy as np
+import requests
+from PIL import Image
+
+from sklearn.neighbors import NearestNeighbors
+
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.models import Model
+
+
+LOGIN = os.environ.get('LOGIN')
+PASSWORD = os.environ.get('PASSWORD')
+TIME_DELTA = timedelta(days=182)
+
+
+def add_group(group: str) -> int:
+    """
+    Add a group to the database if it does not already exist.
+
+    :param group: str – The name of the group to be added.
+    :return: int – The ID of the group from the database.
+    """
+    db = sqlite3.connect('data/losty_db.db')
+    cur = db.cursor()
+    try:
+        # Insert the group into the groups table, ignoring if it already exists.
+        cur.execute('INSERT OR IGNORE INTO groups (name) VALUES (?);', (group,))
+        db.commit()
+
+        # Retrieve the group_id of the newly inserted or existing group.
+        cur.execute('SELECT group_id FROM groups WHERE name = ?;', (group,))
+        group_id = cur.fetchone()[0]
+        return group_id
+    except Exception as e:
+        print(f"Error while adding a group: {e}")
+        db.rollback()
+    finally:
+        cur.close()
+        db.close()
+
+
+def add_post(row: tuple) -> int:
+    """
+    Add a post record to the database.
+
+    :param row: tuple – A tuple containing the following values:
+        — group_id: int – The ID of the group the post belongs to.
+        — shortcode: str – The shortcode of the post.
+        — date: datetime.datetime – The date the post was published.
+        — caption: str – The caption of the post.
+    :return: int – The ID of the newly inserted post.
+    """
+    db = sqlite3.connect('data/losty_db.db')
+    cur = db.cursor()
+    try:
+        # Insert the post into the posts table.
+        cur.execute('''
+            INSERT INTO posts (group_id, shortcode, date, caption) VALUES (?, ?, ?, ?);
+        ''', row)
+        post_id = cur.lastrowid
+        db.commit()
+        return post_id
+    except Exception as e:
+        print(f"Error while adding a post: {e}")
+        db.rollback()
+    finally:
+        cur.close()
+        db.close()
+
+
+def add_image(post_id: int, image_path: str, embedding: list) -> None:
+    """
+    Add an image record to the database.
+
+    :param post_id: int – The ID of the associated post.
+    :param image_path: str – The file path to the image.
+    :param embedding: list – The embedding of the image as a list.
+    """
+    db = sqlite3.connect('data/losty_db.db')
+    cur = db.cursor()
+    try:
+        # Insert the image record into the images table.
+        cur.execute('''
+            INSERT INTO images (post_id, image_path, embedding) VALUES (?, ?, ?);
+        ''', (post_id, image_path, json.dumps(embedding)))
+        db.commit()
+    except Exception as e:
+        print(f"Error while adding an image: {e}")
+        db.rollback()
+    finally:
+        cur.close()
+        db.close()
+
+
+def check_shortcode(shortcode: str) -> bool:
+    """
+    Check if a shortcode exists in the database.
+
+    :param shortcode: str – The shortcode to be checked.
+    :return: bool – True if the shortcode exists, False otherwise.
+    """
+    db = sqlite3.connect('data/losty_db.db')
+    cur = db.cursor()
+    try:
+        # Check if the shortcode exists in the database.
+        cur.execute('SELECT EXISTS (SELECT 1 FROM posts WHERE shortcode LIKE ?) AS FOUND;', (f'%{shortcode}%',))
+        return cur.fetchone()[0]
+    except Exception as e:
+        print(f"Error while checking shortcode's from the database: {e}")
+        db.rollback()
+    finally:
+        cur.close()
+        db.close()
+
+
+def cleanup_data() -> None:
+    """
+    Cleans up old image and post data from the database and file system.
+
+    """
+    db = sqlite3.connect('data/losty_db.db')
+    cur = db.cursor()
+    try:
+        zero_date = datetime.now(tz=timezone.utc) - TIME_DELTA
+        cur.execute("""
+            SELECT i.post_id, MIN(i.image_path) AS image_path
+            FROM images AS i
+            INNER JOIN posts AS p ON i.post_id = p.post_id
+            WHERE p.date < ?
+            GROUP BY i.post_id;
+        """, (zero_date.strftime('%Y-%m-%d %H:%M:%S'),))
+        rows = cur.fetchall()
+        if rows:
+            # Delete images and posts from the database.
+            cur.execute(f"DELETE FROM images WHERE post_id IN ({','.join('?' * len(rows))})", [row[0] for row in rows])
+            cur.execute(f"DELETE FROM posts WHERE post_id IN ({','.join('?' * len(rows))})", [row[0] for row in rows])
+            # Remove image files from the file system.
+            for row in rows:
+                directory = os.path.dirname(row[1])
+                base_name = os.path.basename(row[1]).split('_UTC')[0]
+                for file_name in os.listdir(directory):
+                    if file_name.startswith(base_name):
+                        file_path = os.path.join(directory, file_name)
+                        os.remove(file_path)
+        db.commit()
+    except Exception as e:
+        print(f"Error during cleanup: {e}")
+        db.rollback()
+    finally:
+        cur.close()
+        db.close()
+
+
+class LostyFinder:
+    """
+    Class for updating database and finding matches for an image.
+    """
+
+    def __init__(self):
+        """
+        Initialize the class.
+
+        :param self.groups: list – List of group names.
+        :param self.loader: Instaloader object.
+        :param self.model: ResNet50 model.
+        :param self.knn: NearestNeighbors object.
+        """
+        self.groups = ['lost_found_pets_almaty', 'poteryashki_almaty', 'almaty_pomosh_zhivotnym', 'aulau_kyzmeti']
+        self.loader = instaloader.Instaloader()
+        self.login()
+
+        base_model = ResNet50(weights='imagenet')
+        self.model = Model(inputs=base_model.input, outputs=base_model.layers[-2].output)
+        self.knn = NearestNeighbors(metric='cosine')
+
+    def login(self, relogin: bool = False) -> None:
+        """
+        Login to instagram and saving the session file.
+
+        :param relogin: bool - If True, a relogin will be done with the session file saved.
+        """
+        try:
+            if not relogin and os.path.exists('data/session-losty.pets'):
+                self.loader.load_session_from_file(LOGIN, 'data/session-inst')
+            else:
+                self.loader.login(user=LOGIN, passwd=PASSWORD)
+                self.loader.save_session_to_file('data/session-inst')
+                print('Successful re-login attempt')
+        except Exception as e:
+            print(f"Unsuccessful re-login attempt: {e}")
+
+    def get_embedding(self, image_path: str) -> list:
+        """
+        Get the embedding of the given image using the ResNet50 model.
+
+        :param image_path: str – The file path to the image.
+        :return: list – The embedding of the image as a list.
+        """
+        try:
+            img = image.load_img(image_path, target_size=(224, 224))
+            img_data = image.img_to_array(img)
+            img_data = np.expand_dims(img_data, axis=0)
+            img_data = preprocess_input(img_data)
+            embedding = self.model.predict(img_data, verbose=0).flatten()
+            return embedding.tolist()
+        except Exception as e:
+            print(f"Error while getting embedding: {e}")
+            return []
+
+    def knn_refit(self) -> None:
+        """
+        Refit the KNN model using embeddings from the database.
+
+        :return: None.
+        """
+        db = sqlite3.connect('data/losty_db.db')
+        cur = db.cursor()
+        try:
+            embeddings_data = []
+            # Retrieve all embeddings from the images table.
+            cur.execute('SELECT embedding FROM images')
+            for row in cur.fetchall():
+                # Deserialize the embedding from JSON format and append to list.
+                embedding = json.loads(row[0])
+                embeddings_data.append(embedding)
+
+            # If embeddings are found, fit the KNN model.
+            if embeddings_data:
+                embeddings_array = np.array(embeddings_data)
+                self.knn.fit(embeddings_array)
+
+        except Exception as e:
+            print(f"An error occurred while refitting KNN model: {e}")
+            db.rollback()
+        finally:
+            cur.close()
+            db.close()
+
+    def find_matches(self, image_path: str) -> dict:
+        """
+        Find matches for the given image by calculating the distances between its embedding
+        and the embeddings of all images in the database. The matches are paginated and returned
+        as a dictionary where each key is a page number and the value is a dictionary where
+        each key is a shortcode, and the value is a list of the following format:
+        [image_path, date, image_path, match_percentage].
+
+        :param image_path: The file path to the image to find matches for.
+        :return: A dictionary of matches paginated by page number.
+        """
+
+        # Retrieve the embedding of the given image.
+        input_embedding = self.get_embedding(image_path)
+        input_embedding_array = np.array(input_embedding).reshape(1, -1)
+        try:
+            # Calculate the distances between the input embedding and all embeddings in the database.
+            distances, indices = self.knn.kneighbors(input_embedding_array, n_neighbors=1000)
+        except Exception as e:
+            print(f"An error occurred while finding matches: {e}")
+            return {}
+
+        # Initialize an empty dictionary to store the matches.
+        pages = {}
+        # Define the page size.
+        page_size = 10
+        # Initialize the page number.
+        current_page = 1
+
+        db = sqlite3.connect('data/losty_db.db')
+        cur = db.cursor()
+
+        try:
+            # Iterate over the indices of the nearest neighbors.
+            for index in indices[0]:
+                index = int(index)
+                # Retrieve the post_id and image_path of the current nearest neighbor.
+                cur.execute('''
+                        SELECT post_id, image_path FROM images LIMIT 1 OFFSET ?;
+                    ''', (index,))
+                match = cur.fetchone()
+                if match:
+                    post_id, image_path = match
+                    # Retrieve the shortcode and date of the post.
+                    cur.execute('''
+                            SELECT shortcode, date FROM posts WHERE post_id = ?;
+                        ''', (post_id,))
+                    row = cur.fetchone()
+                    shortcode = row[0]
+                    date = row[1]
+                    # If the current page is not in the pages dictionary, add it.
+                    if current_page not in pages:
+                        pages[current_page] = {}
+                    # If the shortcode is not already in the pages dictionary, add it.
+                    if not any(shortcode in d for d in pages.values()):
+                        # Calculate the match percentage.
+                        match_percentage = (1 - distances[0][list(indices[0]).index(index)])
+                        # Add the match to the pages dictionary.
+                        pages[current_page][shortcode] = [image_path.split('\\')[0], date, image_path, match_percentage]
+                    # If the current page is full, increment the page number.
+                    if len(pages[current_page]) >= page_size:
+                        current_page += 1
+                        if current_page == 11:
+                            break
+            return pages
+        except Exception as e:
+            print(f"Error while finding matches: {e}")
+            db.rollback()
+            return {}
+        finally:
+            cur.close()
+            db.close()
+
+    def update_data(self, start_date: datetime = None) -> None:
+        """
+        Update the database with new data from the given groups.
+
+        :param start_date: datetime.datetime - The start date of the posts to be downloaded.
+        :return: None.
+        """
+
+        if start_date is None:
+            start_date = datetime.now(tz=timezone.utc) - TIME_DELTA
+
+        for group in self.groups:
+            try:
+                # Load profile from Instagram.
+                profile = instaloader.Profile.from_username(self.loader.context, group)
+
+                # Save profile picture if it doesn't exist.
+                if not os.path.exists(f"{group}/{group}_profile_pic.jpg"):
+                    profile_pic_url = profile.profile_pic_url_no_iphone
+                    image_b = requests.get(profile_pic_url).content
+                    image = Image.open(io.BytesIO(image_b))
+                    image.save(f"{group}/{group}_profile_pic.jpg")
+
+                # Download posts from the profile.
+                self.loader.posts_download_loop(
+                    posts=profile.get_posts(),
+                    target=profile.username,
+                    fast_update=True,
+                    takewhile=lambda post: post.date_utc > start_date,
+                    possibly_pinned=3
+                )
+            except Exception as e:
+                print(f"Attempt to update database aborted: authorization error ({e})")
+                self.knn_refit()
+                self.login(relogin=True)
+                return
+
+            # Add the group to the database.
+            group_id = add_group(group)
+
+            try:
+                # Process each file in the group's directory.
+                for file_name in os.listdir(f"{group}/"):
+                    # Delete .mp4 files.
+                    if file_name.endswith('.mp4'):
+                        os.remove(os.path.join(group, file_name))
+                        continue
+
+                    if file_name.endswith('.json.xz'):
+                        with lzma.open(os.path.join(group, file_name), 'rt') as file:
+                            data = json.load(file)['node']
+
+                        shortcode = data['shortcode']
+
+                        # Check if the post is already in the database.
+                        if not check_shortcode(shortcode):
+                            date = datetime.fromtimestamp(data['date'], tz=timezone.utc)
+                            caption = data['caption']
+                            filename = date.strftime('%Y-%m-%d_%H-%M-%S_UTC')
+
+                            # Add the post to the database.
+                            post_id = add_post((group_id, shortcode, date, caption))
+
+                            # Determine the type and process accordingly.
+                            if data['__typename'] in ('GraphImage', 'GraphVideo'):
+                                image_path = os.path.join(group, f"{filename}.jpg")
+                                embedding = self.get_embedding(image_path)
+                                add_image(post_id, image_path, embedding)
+                            elif data['__typename'] == 'GraphSidecar':
+                                for i in range(1, len(data['edge_sidecar_to_children']['edges']) + 1):
+                                    image_path = os.path.join(group, f"{filename}_{i}.jpg")
+                                    embedding = self.get_embedding(image_path)
+                                    add_image(post_id, image_path, embedding)
+                            else:
+                                continue
+            except Exception as e:
+                print(f"Error processing files for group {group}: {e}")
+
+            # Log successful update.
+            print(f"Data from group @{group} updated")
+
+        # Refit KNN model and cleanup old data.
+        self.knn_refit()
+        cleanup_data()
